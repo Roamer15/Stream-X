83058a314e7d350a6a86b5de31c7ece4df1939f1
Roamer15 <ianseeksbeta@gmail.com>
2025-04-27 22:46:11 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,7 +24,7 @@ export default function Home() {
         <Navbar />
         <div className="movie-detail">
           <img
-            src="https://s3-alpha-sig.figma.com/img/dffb/b860/559f834e0275c009608027ccae5ee787?Expires=1743984000&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=rcY4AXVThttS0O2kEO7-lWaUpvIOE4l5p9cyyjl5i3o0VXCFvs1KbBdbNVOaGfszHhcS9TxBvVm~QKZU9OSgSatkthIqD-O4-I-HWnK5K01NSckUzr2VeE68wheN4meZb-hub46fDlGfaA9uD7ZMW0yXVlvb0KZhm0ZYN0Rlm3-lZL4vXjZQVVkaw-AowWvZoJAQx20CHImnOxXlOM~7AGyCEBz4kWdR92bLpAxOafT7Yz3wp7yAXO19lvHtjy-gqFkiefKB-IlxyqA6ugv1XuJK-5sa2sMlDXDMBudJNRv0egb49RjpZwFXT~w4EBmRtB5dIJ5OqlU732nvlXdmNA__"
+            src="https://wallpapers.com/images/featured/spider-man-background-lyuziw9ra4dw36ai.webp"
             alt="spider"
           />
           <div className="initial">
